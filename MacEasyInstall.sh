05aa3766a3a845db6b612168fda751b7725a697a
Liroy van Hoewijk <4643445+liroyvh@users.noreply.github.com>
2021-06-23 05:40:31 +0200

    Create MacEasyInstall.sh
    This file simply triggers the chained commands to do everything on MacOS, for the less tech-savvy. Makes it easier than having to ensure people copy/paste the chain of commands on a single line. Run at own risk.

---
+++ MacEasyInstall.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#This file simply triggers the chained commands to do everything on MacOS, for the less tech-savvy. Run at own risk.
+git clone https://github.com/liroyvh/signal-export.git && cd signal-export && pip install -r requirements.txt && brew install openssl sqlcipher wkhtmltopdf && ./sigexport.py EXPORT && cd EXPORT && echo "Now generating PDF files..." && mkdir -p pdf && find . -maxdepth 2 -name '*.html' -exec sh -c 'for f; do wkhtmltopdf --enable-local-file-access "$f" "./pdf/$(basename "$(dirname "$f")").pdf"; done' _ {} + && open . -a Finder
